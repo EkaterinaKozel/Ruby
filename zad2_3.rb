d66106c29384f6f66a4313c4697edc49953d4f37
EkaterinaKozel <66666949+EkaterinaKozel@users.noreply.github.com>
2022-03-01 23:01:43 +0300

    zad1.1

--- zad2_3.rb
+++ zad2_3.rb
@@ -1,3 +1,13 @@
 puts "Привет, #{ARGV[0]}"
 puts "Какой твой любимый ЯП?"
 language = STDIN.gets.chomp
+case language
+ when "ruby"
+ puts "подлиза"
+ when "python"
+ puts "!"
+ when "c++"
+ puts "."
+ else
+ puts "Неизвестный мне язык"
+ end
