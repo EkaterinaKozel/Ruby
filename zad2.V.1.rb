ab79de2ba7d03a7791233b78b474720f43c822a6
EkaterinaKozel <66666949+EkaterinaKozel@users.noreply.github.com>
2022-03-29 19:55:55 +0300

    2.v.1

--- zad2.V.1.rb
+++ zad2.V.1.rb
@@ -1,4 +1,4 @@
-def compire(num1, num2)
+def coprime(num1, num2)
     if num1>num2
         num1, num2 = num2, num1
     end
@@ -9,3 +9,14 @@ def compire(num1, num2)
         end
     end
 end
+def method1(num1)
+    puts "Сравниваемое число: "
+    num2 = gets.chomp
+    k = 0
+        if coprime(num1, num2)
+            k+=1
+        end
+        return k 
+    end 
+num1 = gets.chomp
+puts method1(num1)
