3e9f34e0228768ded57ef1bdef75db612bf313f2
EkaterinaKozel <66666949+EkaterinaKozel@users.noreply.github.com>
2022-03-29 20:28:11 +0300

    zad2.v.2

--- zad2.V.2.rb
+++ zad2.V.2.rb
@@ -1,4 +1,5 @@
 def sum_3(num)
+    s=0
     while num != 0
         a = num % 10
         if a % 3 == 0
@@ -6,3 +7,7 @@ def sum_3(num)
         end
         num = num/10
     end
+    return s
+end
+num=gets.chomp
+puts sum_3(num)
